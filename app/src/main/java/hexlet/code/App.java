8df6deda59f00e15939bdf883f55622531a929d1
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-01-06 02:24:19 +0400

    Initialize picocli util settings

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,36 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "gendiff 0.0.1",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<String> {
+    @Parameters(description = "Path to original file",
+                paramLabel = "originalFilePath")
+    private String originalFilePath;
+    @Parameters(description = "Path to file to compare with original source",
+                paramLabel = "comparableFilePath")
+    private String comparableFilePath;
+    @Option(names = {"-f", "--format"},
+            description = "output format [default: stylish]",
+            paramLabel = "format")
+    private String format = "stylish";
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public String call() throws Exception {
+        String callResult = Differ.generate(originalFilePath, comparableFilePath);
+        System.out.println(callResult);
+        return callResult;
     }
 }
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,7 @@
+package hexlet.code;
+
+public class Differ {
+    public static String generate(String originalFilePath, String comparableFilePath) {
+        return "";
+    }
+}
