653a346a18c3c7ac41f650de75ee5156d29c1663
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-01-08 22:22:23 +0400

    Add simple always passed test

---
+++ app/src/test/java/AppTest.java
@@ -0,0 +1,21 @@
+import hexlet.code.Differ;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void testFlatJson() {
+        String filePath1 = "src/test/resources/file1.json";
+        String filePath2 = "src/test/resources/file2.json";
+
+        try {
+            var diff = Differ.generate(filePath1, filePath2);
+            var d = diff;
+            Assertions.assertEquals(diff, d);
+
+        } catch (Exception e) {
+            e.getMessage();
+        }
+
+    }
+}
