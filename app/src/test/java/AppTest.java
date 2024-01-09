301090bf2e6b231f9a7ecefaddf419d11cc333a2
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-01-10 00:55:31 +0400

    Rework fixtures directory path in AppTest

--- app/src/test/java/AppTest.java
+++ app/src/test/java/AppTest.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.Test;
 public class AppTest {
     @Test
     public void testFlatJson() {
-        String filePath1 = "src/test/resources/file1.json";
-        String filePath2 = "src/test/resources/file2.json";
+        String filePath1 = "src/test/resources/fixtures/file1.json";
+        String filePath2 = "src/test/resources/fixtures/file2.json";
 
         try {
             var diff = Differ.generate(filePath1, filePath2);
