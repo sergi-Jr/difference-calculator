d82cc9b593203f9279ad4eb9a1a899bbe2e010b0
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-01-14 01:34:04 +0400

    Create serializer

---
+++ app/src/main/java/hexlet/code/PrefixedPairDataSerializer.java
@@ -0,0 +1,30 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import hexlet.code.model.PrefixedPairData;
+
+import java.io.IOException;
+
+public class PrefixedPairDataSerializer extends StdSerializer<PrefixedPairData> {
+
+    public PrefixedPairDataSerializer() {
+        this(null);
+    }
+
+    public PrefixedPairDataSerializer(Class<PrefixedPairData> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(PrefixedPairData prefixedPairData,
+                          JsonGenerator jsonGenerator,
+                          SerializerProvider serializerProvider)
+            throws IOException {
+        String prefixKey = prefixedPairData.getPrefix().concat(prefixedPairData.getKey());
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeObjectField(prefixKey, prefixedPairData.getValue());
+        jsonGenerator.writeEndObject();
+    }
+}
