f9424f82e6d5e6e02fb1452737ff66d67763f4ee
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-01-14 01:34:22 +0400

    Add json formatter

---
+++ app/src/main/java/hexlet/code/formatters/JsonFormatter.java
@@ -0,0 +1,29 @@
+package hexlet.code.formatters;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import hexlet.abstracts.IFormatter;
+import hexlet.abstracts.MapperFactory;
+import hexlet.code.PrefixedPairDataSerializer;
+import hexlet.code.model.PrefixedPairData;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Set;
+
+public class JsonFormatter implements IFormatter {
+    private ObjectMapper mapper;
+
+    public JsonFormatter() throws IOException {
+        mapper = MapperFactory.getMapper("json");
+        SimpleModule module = new SimpleModule("PrefixedPairDataSerializer");
+        module.addSerializer(PrefixedPairData.class, new PrefixedPairDataSerializer());
+        mapper.registerModule(module);
+    }
+    @Override
+    public String makeOutputString(Set<PrefixedPairData> dataSet) throws IOException {
+        StringWriter writer = new StringWriter();
+        mapper.writeValue(writer, dataSet);
+        return writer.toString().replaceAll("},", "}," + System.lineSeparator());
+    }
+}
